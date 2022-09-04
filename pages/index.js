ef72177cca5661cfdceafaf47ee3caabc38634e9
ATT-OAST <ms6926@att.com>
2022-09-04 10:31:25 -0700

    changed query

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
-import { useState } from "react";
+import {
+  useState
+} from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [textInput, setTextInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -13,35 +15,61 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({
+        text: textInput
+      }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    setTextInput("");
   }
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+  return ( <
+    div >
+    <
+    Head >
+    <
+    title > OpenAI Quickstart < /title> <
+    link rel = "icon"
+    href = "/dog.png" / >
+    <
+    /Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
+    <
+    main className = {
+      styles.main
+    } >
+    <
+    img src = "/dog.png"
+    className = {
+      styles.icon
+    }
+    /> <
+    h3 > Name my pet < /h3> <
+    form onSubmit = {
+      onSubmit
+    } >
+    <
+    input type = "text"
+    name = "text"
+    placeholder = "Enter text to parse"
+    value = {
+      textInput
+    }
+    onChange = {
+      (e) => setTextInput(e.target.value)
+    }
+    /> <
+    input type = "submit"
+    value = "Find Entities" / >
+    <
+    /form> <
+    div className = {
+      styles.result
+    } > {
+      result
+    } < /div> < /
+    main > <
+    /div>
   );
 }
